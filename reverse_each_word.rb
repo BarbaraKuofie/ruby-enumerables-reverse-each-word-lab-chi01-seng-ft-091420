362d245503a1f889cbf5fd7726b388df0f2f895b
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-08 19:55:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,12 @@
 def reverse_each_word(string)
-  string = string.split('')
+  new_string = string.split('')
   reversed_string = [ ]
-  reversed_string << string.each {|element|element.reverse!}
+  reversed_string << new_string.each {|element|element.reverse!}
   reversed_string.join("")
 end 
+
+def reverse_each_word(string)
+  arr = string.split(" ")
+  new_arr = arr.collect {|a| a.reverse}
+  new_arr.join(" ")
+end
